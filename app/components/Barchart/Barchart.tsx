9c4accb98707318efe74cfd39f3815711650e6b9
Gwen Wendell <gwen@thecodingdesigner.com>
2024-06-12 18:09:56 -0400

    🟢 app/components/Barchart/Barchart.tsx

---
+++ app/components/Barchart/Barchart.tsx
@@ -0,0 +1,38 @@
+'use client';
+import React, { useEffect, useRef } from 'react';
+import * as d3 from "d3";
+
+// import styles from './Barchart.css';
+
+export interface LinePlotProps {
+  data: any;
+  prop?: string;
+  width: number
+  height: number
+  marginTop: number
+  marginRight: number
+  marginBottom: number
+  marginLeft: number
+}
+
+export default function LinePlot({
+  data,
+  width = 640,
+  height = 400,
+  marginTop = 20,
+  marginRight = 20,
+  marginBottom = 20,
+  marginLeft = 20
+}: LinePlotProps) {
+  const x = d3.scaleLinear([0, data.length - 1], [marginLeft, width - marginRight]);
+  const y = d3.scaleLinear(d3.extent(data), [height - marginBottom, marginTop]);
+  const line = d3.line((d, i) => x(i), y);
+  return (
+    <svg width={width} height={height}>
+      <path fill="none" stroke="currentColor" strokeWidth="1.5" d={line(data)} />
+      <g fill="white" stroke="currentColor" strokeWidth="1.5">
+        {data.map((d, i) => (<circle key={i} cx={x(i)} cy={y(d)} r="2.5" />))}
+      </g>
+    </svg>
+  );
+}
