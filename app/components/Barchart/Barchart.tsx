340a8063e389b4d79d6e16c58b5b007b2ef22f4b
Gwen Wendell <gwen@thecodingdesigner.com>
2024-06-12 18:11:15 -0400

    🛠️ app/components/Barchart/Barchart.tsx -> Added useRef for gx, gy 🛠️ app/components/Barchart/Barchart.tsx -> Added useEffect for axes 🛠️ app/components/Barchart/Barchart.tsx -> Updated SVG elements

--- app/components/Barchart/Barchart.tsx
+++ app/components/Barchart/Barchart.tsx
@@ -24,14 +24,30 @@ export default function LinePlot({
   marginBottom = 20,
   marginLeft = 20
 }: LinePlotProps) {
-  const x = d3.scaleLinear([0, data.length - 1], [marginLeft, width - marginRight]);
+  const gx = useRef();
+  const gy = useRef();
+  const x = d3.scaleLinear(
+    [0, data.length - 1],
+    [marginLeft, width - marginRight]
+  );
   const y = d3.scaleLinear(d3.extent(data), [height - marginBottom, marginTop]);
   const line = d3.line((d, i) => x(i), y);
+  useEffect(() => void d3.select(gx.current).call(d3.axisBottom(x)), [gx, x]);
+  useEffect(() => void d3.select(gy.current).call(d3.axisLeft(y)), [gy, y]);
   return (
     <svg width={width} height={height}>
-      <path fill="none" stroke="currentColor" strokeWidth="1.5" d={line(data)} />
-      <g fill="white" stroke="currentColor" strokeWidth="1.5">
-        {data.map((d, i) => (<circle key={i} cx={x(i)} cy={y(d)} r="2.5" />))}
+      <g ref={gx} transform={`translate(0,${height - marginBottom})`} />
+      <g ref={gy} transform={`translate(${marginLeft},0)`} />
+      <path
+        fill="none"
+        stroke="currentColor"
+        stroke-width="1.5"
+        d={line(data)}
+      />
+      <g fill="white" stroke="currentColor" stroke-width="1.5">
+        {data.map((d, i) => (
+          <circle key={i} cx={x(i)} cy={y(d)} r="2.5" />
+        ))}
       </g>
     </svg>
   );
