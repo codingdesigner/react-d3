e63a6ff2c563cb1e7f5ee90937d8d79fe77514d3
Gwen Wendell <gwen@thecodingdesigner.com>
2024-06-14 00:53:15 -0400

    🛠️ app/components/Menu/Menu.module.css -> Updated styles, added flex properties
    🛠️ app/components/Menu/Menu.tsx -> Changed link to Line Chart page

--- app/components/Menu/Menu.tsx
+++ app/components/Menu/Menu.tsx
@@ -6,7 +6,7 @@ export function Menu() {
   return (
     <ul className={styles.Menu}>
       <li><a href='/'>Home</a></li>
-      <li><a href='/next'>Next</a></li>
+      <li><a href='/pages/line-chart'>Line Chart</a></li>
     </ul>
   );
 }
