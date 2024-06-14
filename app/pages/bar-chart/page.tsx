d9c323802387c0a47855774e19b7927d945335b9
Gwen Wendell <gwen@thecodingdesigner.com>
2024-06-14 01:19:16 -0400

    🛠️ app/pages/bar-chart/page.tsx -> Replaced LinePlot with BarChart, removed commented code, and simplified main tag.

--- app/pages/bar-chart/page.tsx
+++ app/pages/bar-chart/page.tsx
@@ -3,20 +3,14 @@
 import { useState } from "react";
 import * as d3 from "d3";
 
-// import { LinePlot } from "../../components/LinePlot";
+import { BarChart } from "@/app/components/BarChart";
 
 export default function Home() {
-  // const [data, setData] = useState(() => d3.ticks(-2, 2, 200).map(Math.sin));
-
-  // function onMouseMove(event: any) {
-  //   const [x, y] = d3.pointer(event);
-  //   setData(data.slice(-200).concat(Math.atan2(x, y)));
-  // }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <h1>Bar Chart</h1>
       <div>
-        {/* <LinePlot data={data} /> */}
+        <BarChart/>
       </div>
     </main>
   );
