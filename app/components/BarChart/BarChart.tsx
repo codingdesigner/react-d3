0b7d036a5ba833e85e51f29a0518725921b754d2
Gwen Wendell <gwen@thecodingdesigner.com>
2024-06-14 01:39:04 -0400

    🛠️ app/components/BarChart/BarChart.tsx -> Indentation fixes

--- app/components/BarChart/BarChart.tsx
+++ app/components/BarChart/BarChart.tsx
@@ -29,10 +29,10 @@ export function BarChart({ prop = 'default value' }: BarChartProps) {
     d3.csv(
       "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv"
     ).then(function (data) {
-        // Ensure data is correctly typed
-        data.forEach(d => {
-          d.Value = +d.Value; // Convert Value to number
-        });
+      // Ensure data is correctly typed
+      data.forEach(d => {
+        d.Value = +d.Value; // Convert Value to number
+      });
 
       // X axis
       const x = d3
@@ -57,7 +57,7 @@ export function BarChart({ prop = 'default value' }: BarChartProps) {
         .selectAll("mybar")
         .data(data)
         .join("rect")
-          .attr("x", (d) => x(d.Country)!)
+        .attr("x", (d) => x(d.Country)!)
         .attr("y", (d) => y(d.Value))
         .attr("width", x.bandwidth())
         .attr("height", (d) => height - y(d.Value))
