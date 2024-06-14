cf1c75c3122adb5d5d55cc77483f1462956cc03c
Gwen Wendell <gwen@thecodingdesigner.com>
2024-06-14 01:12:14 -0400

    🛠️ app/components/Menu/Menu.tsx -> Added 'Bar Chart' link

--- app/components/Menu/Menu.tsx
+++ app/components/Menu/Menu.tsx
@@ -6,6 +6,7 @@ export function Menu() {
   return (
     <ul className={styles.Menu}>
       <li><a href='/'>Home</a></li>
+      <li><a href='/pages/bar-chart'>Bar Chart</a></li>
       <li><a href='/pages/line-chart'>Line Chart</a></li>
     </ul>
   );
