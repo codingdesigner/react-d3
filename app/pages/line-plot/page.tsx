ef1d1e911ae5c77c74f4e25f60ac94d744f04f86
Gwen Wendell <gwen@thecodingdesigner.com>
2024-06-14 01:18:59 -0400

    🛠️ app/pages/line-plot/page.tsx -> Simplified main tag

--- app/pages/line-plot/page.tsx
+++ app/pages/line-plot/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     setData(data.slice(-200).concat(Math.atan2(x, y)));
   }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <h1>Line Plot</h1>
       <div onMouseMove={onMouseMove}>
         <LinePlot data={data} />
