49a5581d5d6373e3028672ff9040115e7184c1dd
Gwen Wendell <gwen@thecodingdesigner.com>
2024-06-14 01:12:07 -0400

    🛠️ app/pages/line-chart/page.tsx -> Updated header text

--- app/pages/line-chart/page.tsx
+++ app/pages/line-chart/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
+      <h1>Line Chart</h1>
       <div onMouseMove={onMouseMove}>
         <LinePlot data={data} />
       </div>
